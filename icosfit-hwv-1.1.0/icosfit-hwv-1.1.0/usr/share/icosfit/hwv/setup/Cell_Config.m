d4815cb739111c0d342016d523169c28a87078ce
Norton Allen <allen@huarp.harvard.edu>
2022-02-07 16:45:14 -0500

    Cleaning up icosfit-hwv

--- icosfit-hwv-1.1.0/icosfit-hwv-1.1.0/usr/share/icosfit/hwv/setup/Cell_Config.m
+++
@@ -1,6 +0,0 @@
-function cell_cfg = Cell_Config;
-cell_cfg.fsr = 0.027;
-cell_cfg.CavityLength = 10.92;
-cell_cfg.MirrorLoss = 0;
-cell_cfg.N_Passes = 92;
-cell_cfg.CavityFixedLength = 0;
---
+++ icosfit-hwv/gen_icosfit_hwv.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+function nl_error {
+  echo "gen_icosfit_hwv.sh: $*" >&2
+  exit 1
+}
+
+pkgname=icosfit-hwv
+pkgver=1.1.0
+pkgrel=2
+
+verrel=$pkgver-$pkgrel
+pkgverrel=$pkgname-$verrel
+
+# We are actually storing the source here, so we don't want to
+# overwrite it from an archive. We will generate the cygwin
+# archives directly from the source.
+#arch=$pkgname-$pkgver.tar.gz
+#[ -f $arch ] || nl_error Could not locate archive $arch
+#tar -xzf $arch
+
+srcroot=$PWD/$pkgname
+[ -d $srcroot/usr ] || nl_error Could not locate source root
+[ -d x86_64/release/$pkgname ] ||
+  mkdir -p x86_64/release/$pkgname
+
+cd x86_64/release/$pkgname
+tar -Jcf $pkgverrel.tar.xz -C $srcroot usr
+tar -Jcf $pkgverrel-src.tar.xz -C $srcroot usr
+
+cat > $pkgverrel.hint << EOF
+  sdesc: "Setup scripts for using icosfit with HWV"
+  ldesc: "Setup scripts for using icosfit with HWV"
+  category: Science
+  requires: icosfit
+EOF
+
+cat > $pkgverrel-src.hint << EOF
+  sdesc: "Setup scripts for using icosfit with HWV"
+  ldesc: "Setup scripts for using icosfit with HWV"
+  category: Science
+EOF
+
+dest=/usr/src/repo-stage/x86_64/release/$pkgname
+[ -d $dest ] || mkdir $dest
+#cp -uv * $dest
